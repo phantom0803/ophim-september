34e90a67b0f9471cbcad47a70f3c7715188ce21a
phantom0803 <phantom0803>
2022-09-23 22:31:04 +0700

    fix width section_thumb

--- resources/views/september/inc/section/section_thumb.blade.php
+++ resources/views/september/inc/section/section_thumb.blade.php
@@ -3,7 +3,7 @@
     @foreach ($item['data'] ?? [] as $movie)
         <a href="{{ $movie->getUrl() }}" class="overflow-hidden relative group" title="{{ $movie->name ?? '' }}">
             <img style="aspect-ratio: 256/340" src="{{ $movie->thumb_url }}" alt="{{ $movie->name ?? '' }}"
-                class="group-hover:opacity-60 transition-all duration-500 transform group-hover:scale-110" />
+                class="w-full group-hover:opacity-60 transition-all duration-500 transform group-hover:scale-110" />
             <span class="absolute bottom-0 px-2 pb-2 pt-16 bg-gradient-to-t from-main-900 w-full text-main-warning">
                 <div class="font-bold overflow-hidden overflow-ellipsis whitespace-nowrap">{{ $movie->name }}</div>
                 <div class="text-sm text-gray-300 italic overflow-hidden overflow-ellipsis whitespace-nowrap">
